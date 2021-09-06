b5d655d39097c37aca6a36c0a662eed2d79bc81c
Monun <monun1010@gmail.com>
2021-09-07 06:30:40 +0900

    debug true

--- debug-mojang.sh
+++ debug-mojang.sh
@@ -26,7 +26,7 @@ cd "$server_folder" || exit
 if [ ! -f "$start_config" ]; then
   cat <<EOF >$start_config
 server=$server
-debug=false
+debug=true
 debug_port=5005
 backup=false
 force_restart=false
