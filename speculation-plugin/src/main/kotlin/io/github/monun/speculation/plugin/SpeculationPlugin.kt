16790c24438b8be20613da180f771e3be592913f
BaeHyeonWoo <qogusdn1017@naver.com>
2022-01-20 17:30:23 +0900

    :zap: Improved Reseourcepack request

--- speculation-plugin/src/main/kotlin/io/github/monun/speculation/plugin/SpeculationPlugin.kt
+++ speculation-plugin/src/main/kotlin/io/github/monun/speculation/plugin/SpeculationPlugin.kt
@@ -4,6 +4,7 @@ import io.github.monun.kommand.getValue
 import io.github.monun.kommand.kommand
 import io.github.monun.speculation.paper.PaperGameProcess
 import io.github.monun.speculation.paper.PieceColor
+import io.github.monun.speculation.paper.util.setSpeculationResourcePack
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
@@ -60,6 +61,14 @@ class SpeculationPlugin : JavaPlugin() {
                                 throw exception
                             }
                         }
+                    }
+                }
+                then("resourcepack") {
+                    requires { playerOrNull != null }
+                    executes {
+                        player.sendMessage(Component.text("게임 리소스팩을 부여합니다. 화면에 표시된 \"예\" 버튼을 눌러주세요."))
+                        player.sendMessage(Component.text("리소스팩 적용 화면이 표시되지 않았을 시에 서버 리소스팩 여부를 \"사용\" 혹은 \"물어보기\"로 설정해주세요."))
+                        player.setSpeculationResourcePack()
                     }
                 }
             }
